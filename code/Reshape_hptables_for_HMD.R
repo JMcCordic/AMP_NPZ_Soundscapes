556abe4b35ccf54def459712b65f5aa9c7d9d247
JMcCordic <110053627+JMcCordic@users.noreply.github.com>
2026-02-11 16:28:52 -0500

    Update DNE HP

--- code/Reshape_hptables_for_HMD.R
+++ code/Reshape_hptables_for_HMD.R
@@ -119,17 +119,17 @@ hp_new <- hp_data |>
   mutate(
     # summarize presence
     # bal_pres = rowSums(across(ends_with("baleen")), na.rm=TRUE),
-    bal_pres = ifelse(bal_uk > 0, 1, 0),
+    # bal_pres = ifelse(bal_uk > 0, 1, 0),
     # hbw_pres = ifelse(Humpback > 0, 1, 0),
-    # hbw_pres = ifelse(mn_song > 0, 1, 0),
+    hbw_pres = ifelse(mn_song > 0, 1, 0),
     # fish_c_pres = ifelse(Fish_chorus > 0, 1, 0),
     fish_c_pres = ifelse(fish_c > 0, 1, 0),
     # dol_pres = ifelse(Dolphin > 0, 1, 0),
     dol_pres = ifelse(dol_w > 0, 1, 0),
     # mnk_pres = ifelse(Mid_baleen > 0, 1, 0),
-    # mnk_pres = ifelse(minke_sw > 0, 1, 0),
+    mnk_pres = ifelse(minke_sw > 0, 1, 0),
     # spwh_pres = rowSums(across(pm_:Spermwhale_), na.rm = TRUE),
-    spwh_pres = ifelse(pm_frg > 0, 1, 0),
+    # spwh_pres = ifelse(pm_frg > 0, 1, 0),
     # Total_Vessels = rowSums(across(c(CPA, CPA_M,TA,TA_M,TB, TRANSIT, M)), na.rm=TRUE),
     ves_pres = ifelse(Total_Vessels > 0, 1, 0),
     
